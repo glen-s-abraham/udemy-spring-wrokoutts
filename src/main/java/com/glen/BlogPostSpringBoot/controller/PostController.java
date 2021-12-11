15bae01b908e719c2d4f547d2a56265658b62591
glen-s-abraham <glenprojects666@gmail.com>
2021-12-11 12:02:21 +0530

    in memory user basic auth

--- src/main/java/com/glen/BlogPostSpringBoot/controller/PostController.java
+++ src/main/java/com/glen/BlogPostSpringBoot/controller/PostController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -36,6 +37,7 @@ public class PostController {
 	}
 	
 	@PostMapping
+	@PreAuthorize("hasRole('ADMIN')")
 	public ResponseEntity<PostDTO> createPost(@Valid @RequestBody PostDTO post){
 		return new ResponseEntity<>(postService.createPost(post),HttpStatus.CREATED);
 	}
@@ -56,6 +58,7 @@ public class PostController {
 	}
 	
 	@PutMapping("/{id}")
+	@PreAuthorize("hasRole('ADMIN')")
 	public ResponseEntity<PostDTO> updatePostById(
 			@PathVariable Long id,
 			@Valid @RequestBody PostDTO postDto
@@ -65,6 +68,7 @@ public class PostController {
 	}
 	
 	@DeleteMapping("/{id}")
+	@PreAuthorize("hasRole('ADMIN')")
 	public ResponseEntity<String> deletePostById(@PathVariable Long id){
 		postService.deletePostById(id);
 		return new ResponseEntity<>("Post Deleted",HttpStatus.OK);
